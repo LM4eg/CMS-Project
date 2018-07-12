849c49fde0bc5591e2b9f5278dc1c9e26f23c440
lm4egg@gmail.com <admin@2172.s.hostens.cloud>
2018-07-12 20:19:40 +0500

    db

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_HOST", "localhost");
 define("DB_USER", "admin_cms");
-define("DB_PASSWORD", "sR8oep0t0z");
+define("DB_PASSWORD", "tuC4G8MLTM");
 define("DB_NAME", "admin_cms");
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
