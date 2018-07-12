e8d2ec49e8f0175098964aa70405b1baaadba7c0
lm4egg@gmail.com <admin@2172.s.hostens.cloud>
2018-07-12 20:35:49 +0500

    ignore db

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,11 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USER", "admin_cms");
-define("DB_PASSWORD", "tuC4G8MLTM");
-define("DB_NAME", "admin_cms");
+define("DB_USER", "user");
+define("DB_PASSWORD", "password");
+define("DB_NAME", "dbname");
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-if($connection) {
-    echo "we are connected";
-}
+//if($connection) {
+//    echo "we are connected";
+//}
