cb21c50392f6f95402045d5930812f94df599b5e
LM4eg <admin@2172.s.hostens.cloud>
2018-07-19 21:05:23 +0500

    simple search engine

--- search.php
+++ search.php
@@ -15,11 +15,21 @@
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
-                <?php
-                $query = "SELECT * FROM posts";
-                $select_all_posts_query = mysqli_query($connection, $query);
 
-                while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                <?php
+                if(isset($_POST['submit'])) {
+                    $search = $_POST['search'];
+                    $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+                    $search_query = mysqli_query($connection, $query);
+                    if(!$search_query) {
+                        die("QUERY FAILED" . mysqli_error($connection));
+                    }
+                    $count = mysqli_num_rows($search_query);
+                    if($count == 0) {
+                        echo "no results";
+                    } else {
+
+                while($row = mysqli_fetch_assoc($search_query)) {
                     $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
@@ -43,9 +53,10 @@
 
                     <hr>
 
-                    <?php } ?>
-
-
+                    <?php }
+                    }
+                }
+                ?>
 
 
                 <!-- Pager -->
