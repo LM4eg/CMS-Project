8c594cf73f77839b6465aeecc59b1105c33970ff
LM4eg <lm4egg@gmail.com>
2018-08-07 21:11:15 +0500

    add and delete categories from DB

--- admin/categories.php
+++ admin/categories.php
@@ -15,9 +15,26 @@
                         Admin page
                         <small>Author</small>
                     </h1>
-
                     <div class="col-xs-6">
-                        <form action="">
+
+                        <?php
+                        if(isset($_POST['submit'])){
+                            $cat_title = $_POST['cat_title'];
+                            if($cat_title == "" || empty($cat_title)) {
+                                echo "This field should not be empty";
+                            } else {
+                                $query = "INSERT INTO categories(cat_title) ";
+                                $query .= "VALUE('{$cat_title}') ";
+                                $create_category_query = mysqli_query($connection, $query);
+
+                                if(!$create_category_query) {
+                                    die('Query failed' . mysqli_error($connection));
+                                }
+                            }
+                        }
+                        ?>
+
+                        <form action="" method="post">
                             <div class="form-group">
                                 <label for="cat_title">Category Title</label>
                                 <input class="form-control" type="text" name="cat_title">
@@ -28,26 +45,38 @@
                         </form>
                     </div>
                     <div class="col-xs-6">
-                        <?php
-                        $query = "SELECT * FROM categories";
-                        $select_categories = mysqli_query($connection, $query);
-                        ?>
                         <table class="table table-bordered table-hover">
                             <thead>
                             <tr>
                                 <th>ID</th>
                                 <th>Category Title</th>
+                                <th>Delete</th>
                             </tr>
                             </thead>
                             <tbody>
                             <?php
+                            //find all categories query
+                            $query = "SELECT * FROM categories";
+                            $select_categories = mysqli_query($connection, $query);
+
                             while($row = mysqli_fetch_assoc($select_categories)) {
                                 $cat_id = $row['cat_id'];
                                 $cat_title = $row['cat_title'];
                                 echo "<tr>";
                                 echo "<td>{$cat_id}</td>";
                                 echo "<td>{$cat_title}</td>";
+                                echo "<td><a href='categories.php?delete={$cat_id}'>Del</a></td>";
                                 echo "</tr>";
+                            }
+                            ?>
+
+                            <?php
+                            //delete query
+                            if(isset($_GET['delete'])){
+                                $the_cat_id = $_GET['delete'];
+                                $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
+                                $delete_query = mysqli_query($connection, $query);
+                                header("Location: categories.php");
                             }
                             ?>
                             </tbody>
