b3ad621171158a7fc8ee93c3bbce771b34a5d2d4
LM4eg <lm4egg@gmail.com>
2019-04-25 22:41:54 +0500

    edit post

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,40 +1,113 @@
+<?php
+
+if (isset($_GET['p_id'])) {
+    $the_post_id = $_GET['p_id'];
+}
+
+$query = "SELECT * FROM posts WHERE post_id=$the_post_id";
+$select_posts_by_id = mysqli_query($connection, $query);
+
+while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
+    $post_id = $row['post_id'];
+    $post_author = $row['post_author'];
+    $post_title = $row['post_title'];
+    $post_category_id = $row['post_category_id'];
+    $post_status = $row['post_status'];
+    $post_image = $row['post_image'];
+    $post_content = $row['post_content'];
+    $post_tags = $row['post_tags'];
+    $post_comment_count = $row['post_comment_count'];
+    $post_date = $row['post_date'];
+}
+
+if (isset($_POST['update_post'])) {
+    $post_author = $_POST['post_author'];
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_content = $_POST['post_content'];
+    $post_tags = $_POST['post_tags'];
+
+    move_uploaded_file($post_image_temp,"../images/$post_image");
+
+    if (empty($post_image)) {
+        $query = "SELECT * from posts WHERE post_id = $the_post_id ";
+        $select_image = mysqli_query($connection, $query);
+        while ($row = mysqli_fetch_array($select_image)) {
+            $post_image = $row['post_image'];
+        }
+    }
+
+    $query = "UPDATE posts SET ";
+    $query .= "post_title = '{$post_title}', ";
+    $query .= "post_category_id = '{$post_category_id}', ";
+    $query .= "post_date = now(), ";
+    $query .= "post_author = '{$post_author}', ";
+    $query .= "post_status = '{$post_status}', ";
+    $query .= "post_tags = '{$post_tags}', ";
+    $query .= "post_content = '{$post_content}', ";
+    $query .= "post_image = '{$post_image}' ";
+    $query .= "WHERE post_id = {$the_post_id}";
+
+    $update_post = mysqli_query($connection, $query);
+    confirmQuery($update_post);
+}
+?>
+
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="title">Post title</label>
-        <input type="text" class="form-control" name="title">
+        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
     </div>
 
     <div class="form-group">
-        <label for="post_category_id">Post category id</label>
-        <input type="text" class="form-control" name="post_category_id">
+        <label for="post_category">Post category</label>
+        <select name="post_category_id" id="post_category_id">
+            <?php
+            $query = "SELECT * FROM categories";
+            $select_categories = mysqli_query($connection, $query);
+
+            confirmQuery($select_categories);
+
+            while ($row = mysqli_fetch_assoc($select_categories)) {
+                $cat_id = $row['cat_id'];
+                $cat_title = $row['cat_title'];
+
+                echo "<option value='{$cat_id}'>{$cat_title}</option>";
+            }
+            ?>
+        </select>
     </div>
 
     <div class="form-group">
         <label for="author">Post author</label>
-        <input type="text" class="form-control" name="author">
+        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
     </div>
 
     <div class="form-group">
         <label for="post_status">Post status</label>
-        <input type="text" class="form-control" name="post_status">
+        <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
     </div>
 
     <div class="form-group">
-        <label for="image">Post image</label>
+        <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
         <input type="file" name="image">
     </div>
 
     <div class="form-group">
         <label for="post_tags">Post tags</label>
-        <input type="text" class="form-control" name="post_tags">
+        <input value="<?php echo $post_tags; ?>" type="text" class="form-control" name="post_tags">
     </div>
 
     <div class="form-group">
         <label for="post_content">Post content</label>
-        <textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
+        <textarea class="form-control" name="post_content" id="" cols="30"
+                  rows="10"><?php echo $post_content; ?></textarea>
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish">
+        <input class="btn btn-primary" type="submit" name="update_post" value="Update post">
     </div>
 </form>
--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -10,7 +10,8 @@
         <th>Tags</th>
         <th>Comments</th>
         <th>Date</th>
-        <th>Del</th>
+        <th>Edit</th>
+        <th>Delete</th>
     </tr>
     </thead>
     <tbody>
@@ -28,17 +29,27 @@
         $post_tags = $row['post_tags'];
         $post_comment_count = $row['post_comment_count'];
         $post_date = $row['post_date'];
+
         echo "<tr>";
         echo "<td>{$post_id}</td>";
         echo "<td>{$post_author}</td>";
         echo "<td>{$post_title}</td>";
-        echo "<td>{$post_category_id}</td>";
+
+        $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id}";
+        $select_categories_id = mysqli_query($connection, $query);
+        while ($row = mysqli_fetch_assoc($select_categories_id)) {
+            $cat_id = $row['cat_id'];
+            $cat_title = $row['cat_title'];
+            echo "<td>{$cat_title}</td>";
+        }
+
         echo "<td>{$post_status}</td>";
         echo "<td><img width='100' src='../images/{$post_image}' alt='image'></td>";
         echo "<td>{$post_tags}</td>";
         echo "<td>{$post_comment_count}</td>";
         echo "<td>{$post_date}</td>";
-        echo "<td><a href='posts.php?delete={$post_id}'>Delete post</a></td>";
+        echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit post</a></td>";
+        echo "<td><a href='posts.php?delete_post={$post_id}'>Delete post</a></td>";
         echo "</tr>";
     }
     ?>
@@ -47,7 +58,7 @@
 </table>
 
 <?php
-if(isset($_GET['delete'])){
+if (isset($_GET['delete'])) {
     $the_post_id = $_GET['delete'];
     $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
     $delete_query = mysqli_query($connection, $query);
