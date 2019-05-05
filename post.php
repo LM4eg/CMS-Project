57df38cd28b1f28c833e8fc3bcef7f3f43037956
LM4eg <lm4egg@gmail.com>
2019-05-05 21:21:37 +0500

    add comment to post

--- post.php
+++ post.php
@@ -16,13 +16,13 @@
                 <small>Smaller heading text</small>
             </h1>
             <?php
-            if(isset($_GET['p_id'])){
+            if (isset($_GET['p_id'])) {
                 $the_post_id = $_GET['p_id'];
             }
             $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
             $select_all_posts_query = mysqli_query($connection, $query);
 
-            while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+            while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
@@ -50,14 +50,42 @@
 
             <!-- Blog Comments -->
 
+            <?php
+
+            if (isset($_POST['create_comment'])) {
+                $the_post_id = $_GET['p_id'];
+                $comment_author = $_POST['comment_author'];
+                $comment_email = $_POST['comment_email'];
+                $comment_content = $_POST['comment_content'];
+
+                $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
+                $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now())";
+
+                $create_comment_query = mysqli_query($connection, $query);
+                if(!$create_comment_query){
+                    die('Query failed' . mysqli_error($connection));
+                }
+            }
+
+            ?>
+
             <!-- Comments Form -->
             <div class="well">
                 <h4>Leave a Comment:</h4>
-                <form role="form">
+                <form action="" method="post" role="form">
                     <div class="form-group">
-                        <textarea class="form-control" rows="3"></textarea>
+                        <label for="comment_author">Name:</label>
+                        <input type="text" class="form-control" name="comment_author">
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <div class="form-group">
+                        <label for="comment_email">Email:</label>
+                        <input type="email" class="form-control" name="comment_email">
+                    </div>
+                    <div class="form-group">
+                        <label for="textarea">Comment:</label>
+                        <textarea class="form-control" rows="3" name="comment_content"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="create_comment">Submit</button>
                 </form>
             </div>
 
@@ -74,7 +102,9 @@
                     <h4 class="media-heading">Start Bootstrap
                         <small>August 25, 2014 at 9:30 PM</small>
                     </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.
+                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
+                    vulputate fringilla. Donec lacinia congue felis in faucibus.
                 </div>
             </div>
 
@@ -87,7 +117,9 @@
                     <h4 class="media-heading">Start Bootstrap
                         <small>August 25, 2014 at 9:30 PM</small>
                     </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.
+                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
+                    vulputate fringilla. Donec lacinia congue felis in faucibus.
                     <!-- Nested Comment -->
                     <div class="media">
                         <a class="pull-left" href="#">
@@ -97,7 +129,9 @@
                             <h4 class="media-heading">Nested Start Bootstrap
                                 <small>August 25, 2014 at 9:30 PM</small>
                             </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin
+                            commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce
+                            condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                         </div>
                     </div>
                     <!-- End Nested Comment -->
