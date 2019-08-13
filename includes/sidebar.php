977de23a2b4d5d2688c71e59cc3cc123aef7edf2
LM4eg <lm4egg@gmail.com>
2019-08-13 20:23:41 +0500

    login form

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,6 +16,25 @@
         <!-- /.input-group -->
     </div>
 
+    <!-- login form -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" placeholder="Enter username">
+            </div>
+            <div class="input-group">
+                <input name="password" type="password" class="form-control" placeholder="Enter password">
+            <span class="input-group-btn">
+                <button class="btn btn-primary" name="login" type="submit">
+                    Login
+                </button>
+            </span>
+            </div>
+        </form><!-- search form-->
+        <!-- /.input-group -->
+    </div>
+
     <!-- Blog Categories Well -->
     <div class="well">
 
